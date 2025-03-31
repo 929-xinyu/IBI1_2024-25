6ef5f8f878ea179280776a56a1fceffa489414a5
Zhang Xinyu <xinyu1.24@intl.zju.edu.cn>
2025-03-31 18:31:54 +0800

    change Population Sizes.py

--- Practical5/Population Sizes.py
+++ Practical5/Population Sizes.py
@@ -1,29 +1,32 @@
 import matplotlib.pyplot as plt
 
-# 数据准备data preparation
+# create initial list
 uk_countries = ['England', 'Wales', 'Northern Ireland', 'Scotland']
 uk_populations = [57.11, 3.13, 1.91, 5.45]
 
 china_provinces = ['Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu']
 china_populations = [65.77, 41.88, 45.28, 61.27, 85.15]
 
-# 排序sorting
-uk_populations_sorted, uk_countries_sorted = zip(*sorted(zip(uk_populations, uk_countries)))
-china_populations_sorted, china_provinces_sorted = zip(*sorted(zip(china_populations, china_provinces)))
+# sorting
+uk_populations_sorted = sorted(uk_populations)
+china_populations_sorted = sorted(china_populations)
 
-# 打印排序后的列表Print sorted list
-print("Sorted UK populations:", uk_populations_sorted)
-print("Sorted China populations:", china_populations_sorted)
+# Print sorted list
+print("sorted UK populations:", uk_populations_sorted)
+print("sorted China populations:", china_populations_sorted)
 
-# 创建饼图Create a pie chart
-fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+# Create a pie chart for UK populations
+plt.figure(figsize=(8, 8))
+explode_uk = [0.1, 0, 0, 0]  # Highlight England
+colors_uk = ['red', 'blue', 'green', 'yellow']
+plt.pie(uk_populations, labels=uk_countries, autopct='%1.1f%%', startangle=90, explode=explode_uk, colors=colors_uk)
+plt.title('population Distribution in UK Countries')
+plt.show()
 
-# 英国国家人口分布National Population Distribution in the UK
-ax[0].pie(uk_populations_sorted, labels=uk_countries_sorted, autopct='%1.1f%%', startangle=90)
-ax[0].set_title('Population Distribution in UK Countries')
-
-# 中国省份人口分布Population distribution in Chinese provinces
-ax[1].pie(china_populations_sorted, labels=china_provinces_sorted, autopct='%1.1f%%', startangle=90)
-ax[1].set_title('Population Distribution in Zhejiang-neighbouring Provinces')
-
+# Create a pie chart for Chinese provinces
+plt.figure(figsize=(8, 8))
+explode_china = [0, 0, 0, 0.1, 0]  # Highlight Anhui
+colors_china = ['red', 'orange', 'yellow', 'green', 'blue']
+plt.pie(china_populations, labels=china_provinces, autopct='%1.1f%%', startangle=90, explode=explode_china, colors=colors_china)
+plt.title('population Distribution in Zhejiang-neighbouring Provinces')
 plt.show()
