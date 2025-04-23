5705f4308b8b3834ec4b788869df1f106ee27671
Zhang Xinyu <xinyu1.24@intl.zju.edu.cn>
2025-04-23 15:46:52 +0800

    add Practical10

--- Practical10/Global Health Data.py
+++ Practical10/Global Health Data.py
@@ -1,19 +1,13 @@
-
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 设置工作目录
-os.chdir("/Desktop/IBI/IBI_2024-25/Practical10")  # 替换为实际路径
-print(os.getcwd())  # 验证当前路径
-print(os.listdir())  # 检查文件是否存在
-
 # 加载数据
 dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
 
 # 数据检查
-dalys_data.head(5)      # 查看前5行
+dalys_data.head(5)      # 查看前5行=
 dalys_data.info()       # 数据类型和列名
 dalys_data.describe()   # 统计摘要
 
@@ -27,6 +21,7 @@ data_1990 = dalys_data.loc[dalys_data["Year"] == 1990, "DALYs"]
 # 比较国家数据
 uk = dalys_data.loc[dalys_data["Entity"] == "United Kingdom", ["Year", "DALYs"]]
 france = dalys_data.loc[dalys_data["Entity"] == "France", ["Year", "DALYs"]]
+uk.describe()  # 描述性统计
 
 # 绘制英国趋势图
 plt.plot(uk["Year"], uk["DALYs"], "b-", label="UK")
